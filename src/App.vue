1c9b60132dc00c1726d104aaecc4eca358b3dda3
giuseppemaisto <gmaisto98@gmail.com>
2023-01-11 16:11:30 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+    <script>
+    import AppHeader from './components/AppHeader.vue'
+    export default {
+      components:{
+        AppHeader
+      }
+    }
+    </script>
+<template lang="">
+  <AppHeader></AppHeader>
+</template>
+<style lang="">
+  
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+     <script>
+        export default {
+            
+        }
+    </script>
+<template lang="">
+    <header>
+        Primo commit per push struttura 
+    </header>
+</template>
+<style lang="">
+    
+</style>
