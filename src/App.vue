e740e76143ff93fd2278bec54e8d1b3409016fe4
giuseppemaisto <gmaisto98@gmail.com>
2023-01-12 16:46:00 +0100

    da sistemare

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
     import AppHeader from './components/AppHeader.vue'
     import AppFooter from './components/AppFooter.vue'
     import AppContent from './components/AppContent.vue'
+    import CardDC from './components/CardDC.vue'
     export default {
       components:{
         AppHeader,
         AppContent,
+        CardDC,
         AppFooter
       }
     }
@@ -13,6 +15,7 @@
 <template lang="">
   <AppHeader></AppHeader>
   <AppContent></AppContent>
+  <CardDC></CardDC>
   <AppFooter></AppFooter>
 </template>
 <style lang="scss">
---
+++ src/components/CardDC.vue
@@ -0,0 +1,15 @@
+        <script>
+        export default {
+            props:{}
+        }
+        </script>
+<template lang="">
+    <div>
+        <h1>prova dc card</h1>
+    </div>
+</template>
+<style lang="scss">
+       @use '../styles/partials/variable' as *;
+    @use '../styles/partials/mixins' as *;
+
+</style>
